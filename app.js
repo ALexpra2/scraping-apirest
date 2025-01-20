103348a4c51365797be815fa84bce2263dca432e
Alexpra2 <alexpra2@gmail.com>
2025-01-20 09:30:02 +0100

    commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const app = express();
 const fs = require('fs');
 const scrapeNoticias = require('./scraping');
-const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
